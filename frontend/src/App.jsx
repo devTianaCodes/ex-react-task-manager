e873b405677215bf44e5059305277cf26cc2f82a
SwitchTia <oblasser.tatiana@gmail.com>
2026-05-07 15:22:07 +0200

    Feat: Global context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
+import { GlobalProvider } from './context/GlobalContext'
 import AddTask from './pages/AddTask'
 import TaskList from './pages/TaskList'
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <NavLink to="/">Task List</NavLink>
-        <NavLink to="/add-task">Add Task</NavLink>
-      </nav>
-      <Routes>
-        <Route path="/" element={<TaskList />} />
-        <Route path="/add-task" element={<AddTask />} />
-      </Routes>
-    </BrowserRouter>
+    <GlobalProvider>
+      <BrowserRouter>
+        <nav>
+          <NavLink to="/">Task List</NavLink>
+          <NavLink to="/add-task">Add Task</NavLink>
+        </nav>
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/add-task" element={<AddTask />} />
+        </Routes>
+      </BrowserRouter>
+    </GlobalProvider>
   )
 }
 
--- frontend/src/context/GlobalContext.jsx
+++ frontend/src/context/GlobalContext.jsx
@@ -1,9 +1,18 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 const GlobalContext = createContext()
 
 function GlobalProvider({ children }) {
   const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/tasks`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setTasks(data)
+      })
+  }, [])
 
   return (
     <GlobalContext.Provider value={{ tasks, setTasks }}>
