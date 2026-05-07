5eafbfc0e8db8919d3478a394cecad641110b4bf
SwitchTia <oblasser.tatiana@gmail.com>
2026-05-07 21:20:06 +0200

    Feat: taskList table

--- frontend/src/pages/TaskList.jsx
+++ frontend/src/pages/TaskList.jsx
@@ -1,5 +1,32 @@
+import { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalContext'
+
 function TaskList() {
-  return <h1>Task List</h1>
+  const { tasks } = useContext(GlobalContext)
+
+  return (
+    <div>
+      <h1>Task List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Status</th>
+            <th>Creation Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td>{task.title}</td>
+              <td>{task.status}</td>
+              <td>{task.createdAt}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default TaskList
