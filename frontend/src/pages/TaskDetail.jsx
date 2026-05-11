54bc195c97e319c92cdb57b0ee33b76a7f300b4c
SwitchTia <oblasser.tatiana@gmail.com>
2026-05-11 18:53:30 +0200

    Feat: Milestone 7 completed

--- frontend/src/pages/TaskDetail.jsx
+++ frontend/src/pages/TaskDetail.jsx
@@ -1,5 +1,34 @@
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { GlobalContext } from "../context/GlobalContext";
+
+
+// TaskDetail component to display details of a specific task
 function TaskDetail() {
-  return <h1>Task Detail</h1>;
+  const { id } = useParams();// Get the task ID from the URL parameters
+  const { tasks } = useContext(GlobalContext);// Access the tasks from the global context 
+
+  const task = tasks.find((currentTask) => currentTask.id === Number(id));
+
+  if (!task) {
+    return <h1>Task not found</h1>;
+  }
+
+  const handleDelete = () => {
+    console.log("Elimino task: ", task.id);
+  };
+
+  return (
+    <div>
+      <h1>{task.title}</h1>
+      <p>Description: {task.description}</p>
+      <p>Status: {task.status}</p>
+      <p>Created At: {new Date(task.createdAt).toLocaleDateString()}</p>
+      <button type="button" onClick={handleDelete}>
+        Delete Task
+      </button>
+    </div>
+  );
 }
 
 export default TaskDetail;
